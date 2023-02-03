c61e79ba0d9ba36adf495c208625dad325a48dd2
RomainSem <romain_sem@msn.com>
2023-02-03 17:12:39 +0100

    Meilleure façon

--- GénérerDuoAléatoires/Program.cs
+++ GénérerDuoAléatoires/Program.cs
@@ -1,37 +1,37 @@
 ﻿List<string> students = new List<string>() { "Romain", "Solène", "Raph", "Laeticia", "Claire", "Vincent", "Willy", "Arnaud", "Corentin", "Charlie", "Sammy", "Yannick" };
-List<List<string>> lists = new List<List<string>>();
+List<List<string>> groups = new List<List<string>>();
 const int listNb = 6;
 
 Random random = new Random();
 int index; // Nombre aléatoire entre 0 et students.Count
 
-CreateListsInlists(); // Créer les listes dans la liste lists
+CreateListsInlists(); // Créer les listes dans la liste groups
 FillListsWithTwoRandomElements(); // Remplir les listes avec 2 éléments aléatoires
 DisplayListsContents(); // Affiche le contenu de chaque liste
+
+Console.ReadLine();
 
 void CreateListsInlists()
 {
     {
         for (int i = 0; i < listNb; i++)
         {
-            lists.Add(new List<string>());
+            groups.Add(new List<string>());
         }
     }
 }
 
 void FillListsWithTwoRandomElements()
 {
-    for (int i = 0; i < lists.Count; i++)
+    for (int i = 0; i < groups.Count; i++)
     {
-        while (lists[i].Count < 2)
+        while (groups[i].Count < 2)
         {
             index = random.Next(0, students.Count);
+            
+            groups[i].Add(students[index]);
+            students.Remove(students[index]);
 
-            if (!lists[i].Contains(students[index]))
-            {
-                lists[i].Add(students[index]);
-                students.Remove(students[index]);
-            }
         }
     }
 }
@@ -40,7 +40,7 @@ void DisplayListsContents()
 {
     for (int i = 0; i < listNb; i++)
     {
-        Console.WriteLine("List {0}: {1}", i + 1, string.Join(", ", lists[i]));
+        Console.WriteLine("Group {0} : {1} ", i + 1, string.Join(", ", groups[i]));
     }
 }
 
